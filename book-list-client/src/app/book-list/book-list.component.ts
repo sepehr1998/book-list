efb482eaa4a74c55e91a610efa9343ffd1c0c7d6
Sepehr Samadi <samadisepehr@gmail.com>
2025-01-01 20:12:21 +0200

    [WIP]: initial looping with mock data implemented

--- book-list-client/src/app/book-list/book-list.component.ts
+++ book-list-client/src/app/book-list/book-list.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {BookType} from "./book.type";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
 export class BookListComponent {
-
+ books: BookType[] = [
+   { id: 0, author: "author 1", title: "title 1" },
+   { id: 1, author: "author 2", title: "title 2" },
+   { id: 2, author: "author 3", title: "title 3" },
+ ]
 }
