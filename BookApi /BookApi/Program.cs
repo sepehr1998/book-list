3de4fe6b8967e64de20da41e0760b7b200d47735
Sepehr Samadi <samadisepehr@gmail.com>
2025-01-02 14:23:08 +0200

    cors policy configured

--- BookApi /BookApi/Program.cs
+++ BookApi /BookApi/Program.cs
@@ -2,7 +2,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyCors", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("MyCors");
 
 app.MapControllers();
 
